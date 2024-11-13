43c8552938a0d628c82f1f7596a5a344ed9318eb
Esteban PAINPARAY <esteban.painparay@student.junia.com>
2024-11-13 17:37:35 +0100

    debut

--- traffic_light.cpp
+++ traffic_light.cpp
@@ -1,173 +1,145 @@
-// condition_variable::wait (with predicate)
-#include <iostream> // std::cout
-#include <thread>   // std::thread, std::this_thread::yield
+#include <iostream> 
+#include <thread>   
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
 
-using namespace std::chrono_literals;
+enum class FeuEtat { Rouge, Vert };
 
-static const sf::Color Orange(255, 165, 0);
-
-const auto time_transit = 3s;
-const auto time_waiting = 8s;
-
-enum class Traffic_color
-{
-    green = 0,
-    orange = 1,
-    red = 2
-};
-
-Traffic_color operator++(Traffic_color &traffic_color)
-{
-    switch (traffic_color)
-    {
-    case Traffic_color::red:
-        traffic_color = Traffic_color::green;
-        break;
-
-    case Traffic_color::green:
-        std::this_thread::sleep_for(time_transit);
-        traffic_color = Traffic_color::orange;
-        std::this_thread::sleep_for(time_transit);
-        traffic_color = Traffic_color::red;
-    }
-    return traffic_color;
-}
-
-class Traffic_light
-{
+class FeuCirculation {
 private:
-    Traffic_color traffic_color_;
+    FeuEtat etat;
+    std::mutex mtx;
+    std::condition_variable cv;
 
 public:
-    explicit Traffic_light(const Traffic_color &traffic_color) : traffic_color_{traffic_color} {}
-    void operator++()
-    {
-        ++traffic_color_;
+    FeuCirculation() : etat(FeuEtat::Rouge) {}
+
+    void changerEtat() {
+        std::lock_guard<std::mutex> lock(mtx);
+        etat = (etat == FeuEtat::Rouge) ? FeuEtat::Vert : FeuEtat::Rouge;
+        cv.notify_all();
     }
-    void set_traffic_color(const Traffic_color &traffic_color)
-    {
-        traffic_color_ = traffic_color;
+
+    FeuEtat obtenirEtat() {
+        std::lock_guard<std::mutex> lock(mtx);
+        return etat;
     }
-    const Traffic_color &get_traffic_color() const
-    {
-        return traffic_color_;
+
+    void attendreVert() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this] { return etat == FeuEtat::Vert; });
     }
 };
 
-const sf::Color &get_SFML_color(const Traffic_light &traffic_light)
-{
-    switch (traffic_light.get_traffic_color())
-    {
-    case Traffic_color::green:
-        return sf::Color::Green;
-    case Traffic_color::red:
-        return sf::Color::Red;
-    }
-    return Orange;
-}
+class Usager {
+protected:
+    sf::RectangleShape shape;
+    int vitesse;
+    FeuCirculation& feu;
 
-std::ostream &operator<<(std::ostream &os, const Traffic_light &traffic_light)
-{
-    std::string ptr;
-    switch (traffic_light.get_traffic_color())
-    {
-    case Traffic_color::red:
-        ptr = "Red";
-        break;
-    case Traffic_color::green:
-        ptr = "Green";
-        break;
-    case Traffic_color::orange:
-        ptr = "Orange";
+public:
+    Usager(int pos_x, int pos_y, int vitesse_, FeuCirculation& feu_, sf::Color color)
+        : vitesse(vitesse_), feu(feu_) {
+        shape.setSize(sf::Vector2f(20, 20));
+        shape.setPosition(pos_x, pos_y);
+        shape.setFillColor(color);
     }
-    os << ptr;
-    return os;
-}
 
-void run_traffic_light(Traffic_light &traffic_light_master, Traffic_light &traffic_light_slave, std::stop_token stop_token)
-{
-    traffic_light_master.set_traffic_color(Traffic_color::green);
-    traffic_light_slave.set_traffic_color(Traffic_color::red);
-    while (!stop_token.stop_requested())
-    {
-        std::this_thread::sleep_for(time_waiting);
-        if (traffic_light_master.get_traffic_color() == Traffic_color::green)
-        {
-            ++traffic_light_master;
-            ++traffic_light_slave;
-        }
-        else
-        {
-            ++traffic_light_slave;
-            ++traffic_light_master;
+    virtual void deplacer() {
+        while (true) {
+            feu.attendreVert();  // Attendre que le feu soit vert pour avancer
+            shape.move(vitesse, 0);  // Avance sur l'axe des x
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
-}
 
-void print_traffic_light(Traffic_light &traffic_light_master, Traffic_light &traffic_light_slave, std::stop_token stop_token)
-{
-    while (!stop_token.stop_requested())
-    {
-        std::this_thread::sleep_for(1s);
-        std::cout << "Taffic light master : " << traffic_light_master << " Taffic light slave : " << traffic_light_slave << std::endl;
+    sf::RectangleShape& getShape() { return shape; }
+};
+
+// Gestion des feux de chaque direction du carrefour
+void controleFeux(FeuCirculation& feu_NS, FeuCirculation& feu_EO) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        feu_NS.changerEtat();
+        feu_EO.changerEtat();
+        std::cout << "Changement de feux! Nord-Sud : "
+            << (feu_NS.obtenirEtat() == FeuEtat::Vert ? "Vert" : "Rouge")
+            << ", Est-Ouest : "
+            << (feu_EO.obtenirEtat() == FeuEtat::Vert ? "Vert" : "Rouge")
+            << std::endl;
     }
 }
 
-int main()
-{
-    std::stop_source stopping;
-    Traffic_light traffic_light_master{Traffic_color::red};
-    Traffic_light traffic_light_slave{Traffic_color::red};
-    std::jthread thread_traffic_light_master(run_traffic_light,
-                                             std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
+int main() {
+    // Feux de circulation pour les directions Nord-Sud et Est-Ouest
+    FeuCirculation feu_NS;
+    FeuCirculation feu_EO;
 
-    std::jthread write_traffic_light(print_traffic_light,
-                                     std::ref(traffic_light_master), std::ref(traffic_light_slave), stopping.get_token());
+    // Usagers arrivant de chaque direction du carrefour
+    Usager voiture_NS(400, 100, 5, feu_NS, sf::Color::Blue);
+    Usager voiture_EO(100, 300, 5, feu_EO, sf::Color::Green);
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "My window");
+    std::vector<std::thread> threads;
+    threads.emplace_back(&Usager::deplacer, &voiture_NS);
+    threads.emplace_back(&Usager::deplacer, &voiture_EO);
+    threads.emplace_back(controleFeux, std::ref(feu_NS), std::ref(feu_EO));
 
-    float l1 = 350, l2 = 430, size = 800, radius = 10;
-    sf::Vertex line1[] = {sf::Vertex(sf::Vector2f(0, l1)), sf::Vertex(sf::Vector2f(size, l1))};
-    sf::Vertex line2[] = {sf::Vertex(sf::Vector2f(0, l2)), sf::Vertex(sf::Vector2f(size, l2))};
-    sf::Vertex line3[] = {sf::Vertex(sf::Vector2f(l1, 0)), sf::Vertex(sf::Vector2f(l1, size))};
-    sf::Vertex line4[] = {sf::Vertex(sf::Vector2f(l2, 0)), sf::Vertex(sf::Vector2f(l2, size))};
+    // Création de la fenêtre SFML pour afficher la simulation du carrefour
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Simulation de Carrefour");
 
-    sf::CircleShape circle1(radius);
-    circle1.setFillColor(sf::Color::Blue);
-    circle1.setOrigin(circle1.getRadius() / 2, circle1.getRadius() / 2);
-    circle1.setPosition(l2 + radius / 2, l2 + radius / 2);
-    sf::CircleShape circle2(radius);
-    circle2.setFillColor(sf::Color::Green);
-    circle2.setOrigin(circle2.getRadius() / 2, circle2.getRadius() / 2);
-    circle2.setPosition(l2 + radius / 2, l1 - radius);
-
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
-            {
-                stopping.request_stop();
                 window.close();
-                return 0;
-            }
         }
-        window.clear(sf::Color::Black);
 
-        window.draw(line1, 2, sf::Lines);
-        window.draw(line2, 2, sf::Lines);
-        window.draw(line3, 2, sf::Lines);
-        window.draw(line4, 2, sf::Lines);
-        circle1.setFillColor(get_SFML_color(traffic_light_slave));
-        circle2.setFillColor(get_SFML_color(traffic_light_master));
-        window.draw(circle1);
-        window.draw(circle2);
+        // Effacer la fenêtre pour redessiner les éléments
+        window.clear(sf::Color::White);
 
+        // Dessiner les routes
+        sf::RectangleShape routeH(sf::Vector2f(800, 50));
+        routeH.setPosition(0, 275);
+        routeH.setFillColor(sf::Color(128, 128, 128)); // Route horizontale
+
+        sf::RectangleShape routeV(sf::Vector2f(50, 600));
+        routeV.setPosition(375, 0);
+        routeV.setFillColor(sf::Color(128, 128, 128)); // Route verticale
+
+        window.draw(routeH);
+        window.draw(routeV);
+
+        // Dessin des feux de circulation
+        sf::CircleShape feuNS(10);
+        feuNS.setPosition(390, 250);
+        feuNS.setFillColor(feu_NS.obtenirEtat() == FeuEtat::Vert ? sf::Color::Green : sf::Color::Red);
+
+        sf::CircleShape feuEO(10);
+        feuEO.setPosition(350, 300);
+        feuEO.setFillColor(feu_EO.obtenirEtat() == FeuEtat::Vert ? sf::Color::Green : sf::Color::Red);
+
+        window.draw(feuNS);
+        window.draw(feuEO);
+
+        // Dessin des usagers
+        window.draw(voiture_NS.getShape());
+        window.draw(voiture_EO.getShape());
+
+        // Afficher tous les éléments dessinés
         window.display();
+    }
+
+    // Attendre que les threads se terminent
+    for (auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
     }
 
     return 0;
 }
+
+
